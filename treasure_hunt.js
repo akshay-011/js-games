b037337830f42abb90e67d83925c96fec56e8e06
Akshay Kumar <102289107+akshay-011@users.noreply.github.com>
2024-11-22 17:22:05 +0530

    Update treasure_hunt.js

--- treasure_hunt.js
+++ treasure_hunt.js
@@ -1,64 +1,81 @@
-// TODO: name changes, later
+const gameName = `
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+⋆ ˚｡⋆ ˚｡⋆ ˚｡⋆ ˚｡⋆୨୧˚ STEP-10 INTO THE STEPVERSE ˚୨୧⋆｡˚ ⋆｡˚ ⋆｡˚ ⋆｡˚ ⋆
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+`;
+const QUOTE1 = '\n"A game for those who seek to find a way to leave their world behind .・✫・゜・。.・゜✭・"\n';
+const QUOTE2 = "\n\nDo not begin unless you intend to finish otherwise... You'll get the curse of MARKUS HAYAK!!!\n";
+const QUOTE_ONE_PIECE = "\n\n'𝐼𝓉 𝒾𝓈 𝓃𝑜𝓉 𝓉𝒽𝑒 𝓌𝑒𝒶𝓁𝓉𝒽 𝒷𝓊𝓉 𝓂𝒶𝓎𝒷𝑒, 𝓉𝒽𝑒 𝒻𝓇𝒾𝑒𝓃𝒹𝓈 𝓌𝑒 𝓂𝒶𝒹𝑒 𝒶𝓁𝓁 𝒶𝓁𝑜𝓃𝑔, 𝒾𝓈 𝓉𝒽𝑒 𝓇𝑒𝒶𝓁 𝓉𝓇𝑒𝒶𝓈𝓊𝓇𝑒.･:.｡..｡.:'\n";
 
-const gameName = "SOMETHING";
-const QUOTE1 = '\n"A game for those who seek to find a way to leave their world behind .・✫・゜・。..・。.・゜✭・"\n';
-const QUOTE2 = "\n\n'Do not begin unless you intend to finish otherwise You'll get the curse of Jumanji'\n";
-const QUOTE_ONE_PIECE = "\n\n'It is not the wealth but maybe, the friends we made all along, is the real treasure'\n";
-
-const easyQuestion1 = "I killed an entire race of creatures, what am i ?";
-const easyQuestion2 = "I am an indian mathematician born on 476 AD, who am i ?";
-const mediumQuestion1 = "The record good & bad moments ? what am i ?";
-const hardQuestion1 = "I am the expensive thing humans made !, what am i ?";
+const easyQuestion1 = "I caused the extinction of an entire species ? 🗿";
+const easyQuestion2 = "I am an indian🇮🇳 mathematician born on 476 AD, who am i ?";
+const mediumQuestion1 = "The record good😊 & bad😭 moments ? What am i ? (w___ o_ w__ a__ o___)";
+const hardQuestion1 = "I am the expensive thing humans made💸 ! What am i ?";
 
 const easyQue1Answer = "asteroid";
 const easyQue2Answer = "aryabhatta";
-const mediumQue1Answer = "something";
-const hardQue1Answer = "ISS";
+const mediumQue1Answer = "wall of wow and ouch";
+const hardQue1Answer = "is store room";
 
 // sub puzzless
 const puzzle1 = "Your finger has the power!";
 const puzzle2 = "ESTIMATE the EMPLOYEES IPHONE of ARGUMENTS by TROY-SINGHAM";
-const subPuzzle3 = "i am under the sun and above the rainbow";
-const subPuzzle4 = "";
+const puzzle3 = "I'm under the sun and above the rainbow";
+const puzzle4 = `
+Follow the trail of compass direction\n
+\tTo the place written below the rose protection\n
+\t\tThere you'll uncover the secret, it's true\n
+\t\t\tThe answer to complete this quest waits behind the blue ･.｡..｡.:
+`;
 
 const puzzleSolution1 = "energy";
 const puzzleSolution2 = "smart";
-const puzzleSolution3 = "";
-const puzzleSolution4 = "";
+const puzzleSolution3 = "felopzd";
+const puzzleSolution4 = "family";
 
-const description = `
-You might think this floor of thoughtworks is nothing special.
-But... Right in this floor lies a powerful artifact!💎
+//why so many separation?
+const descSegment1 = "You might think this floor of thoughtworks is nothing special.\n";
+const descSegment2 = "But... Right in this floor lies a powerful artifact!💎\n\n\n";
+const descSegment3 = "‧͙⁺˚･༓☾ The STEPSUS PRISM ☽༓･˚⁺‧͙\n\n\n";
+const descSegment4 = "It is the prism that connects all the step interns of different multiverses.\n";
+const descSegment5 = "Players must race across countless worlds🗺️, solving challenges🔎😎 in each universe, \n";
+const descSegment6 = "to find the artifact before dark forces, BAYAN & BILAJ😈🧿 use it to merge the multiverses into chaos😱.\n";
 
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-⋆ ˚｡⋆ ˚｡⋆ ˚｡⋆ ˚｡⋆୨୧˚ The STEPSUS PRISM ˚୨୧⋆｡˚ ⋆｡˚ ⋆｡˚ ⋆｡˚ ⋆!
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+const descriptionPart1 = descSegment1 + descSegment2 + descSegment3;
+const descriptionPart2 = descSegment4 + descSegment5 + descSegment6;
+const description = descriptionPart1 + descriptionPart2;
 
-It is the prism that connects all the step interns of different multiverses👥. 
-Players must race across countless worlds🗺️, solving challenges🔎😎 in each universe, 
-to find the artifact before dark forces😈🧿 use it to merge the multiverses into chaos😱.`
+const instructionHead = "<————«——««——««« INSTRUCTION »»»——»»——»————>\n\n";
+const instruction1 = "✧ each level has a main quest and a sub puzzle\n";
+const instruction2 = "✧ find answer of the main quest, then according to that solve the puzzle\n";
+const instruction3 = "✧ puzzle can only be found at the main quest's location only\n";
+const instruction4 = "✧ puzzle solutions are single word, maybe random letters with no cap or spaces\n";
 
-const WRONG_ANSWER = "❌ Ouch i guess you haven't cracked the hint !!";
-const CORRECT_ANSWER = "🥳 You got it moving on to next question! Hang on\n\n";
+const instruction = instructionHead + instruction1 + instruction2 + instruction3 + instruction4;
+
+const WRONG_ANSWER = "❌ Ouch i guess you haven't cracked the hint yet !!";
+const CORRECT_ANSWER = "🥳 You got it! Now moving on to next question! Hang on\n\n";
 const heading = "♛┈┈┈┈✨┈┈⛧┈┈┈•༶┈•༶💫STEP-10 ACROSS THE STEPVERSE💫༶•┈༶•┈┈⛧┈┈✨┈┈┈┈♛";
-const LEVEL_PASSED = "Woohoo! You've finished your quest in this universe! Moving on to the next one!";
+const LEVEL_PASSED = "Woohoo! You've finished your quest in this universe! Moving on to the next one!\n\n";
+const PUZZLE_SEPERATOR = "⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇⋇⋆✦⋆⋇\n";
+
+const PASS_DECORATION = "\n\n🎉 🥳😘🥳🎉 🥳😘🥳🎉 🥳😘🥳\n";
+const PASS_SENTENCE = "🎉 Congrats for clearing it!\n";
 
 function greetPlayer() {
-  console.log(QUOTE1);
-  console.log(description);
-  console.log(QUOTE2);
+  console.log(gameName, QUOTE1, description, QUOTE2);
 
   confirm("Shall we start the game ? ");
-  console.log("Regardless what do you want we are already started !\n\n");
+  console.clear();
+  console.log("\nRegardless of what you want, we have already started😌 !\n\n");
 }
 
 function greetForPassing() {
-  console.log("🎉 Congrats player for passing\n");
-  console.log("Now you are in medium level, let's start the game\n\n");
+  console.log(PASS_DECORATION, PASS_SENTENCE, LEVEL_PASSED);
 }
 
 function getAnswerAndValidate(answer) {
-  const userAnswer = prompt("What is the Key ? ");
+  const userAnswer = prompt("\nWhat is the Key ? ");
 
   if (userAnswer === answer) {
     console.log(CORRECT_ANSWER);
@@ -70,25 +87,23 @@ function getAnswerAndValidate(answer) {
 }
 
 function printPuzzleMessage(puzzle) {
+  console.log(PUZZLE_SEPERATOR);
   console.log("This is the second part of the quest !!");
   console.log("Solve this puzzle, using answer for the previous one ~");
-  console.log("The puzzle is : \n\n", puzzle);
+  console.log("The puzzle is : \n", puzzle);
 }
 
 function askPuzzle(puzzle, puzzleAnswer) {
   printPuzzleMessage(puzzle);
   getAnswerAndValidate(puzzleAnswer);
-
-  console.log(LEVEL_PASSED);
 }
 
 function askQuestion(question, answer, puzzle, puzzleAnswer) {
-  const decoration = "🍊 Player our secret hint is: \n\n";
-  console.log(decoration, question);
-  console.log("Find the secret key and go to next Level\n");
+  const decoration = "🍊 Player our secret hint is: \n\n ";
+  console.log(decoration, '┈┈┈┈✿', question, '✿┈┈┈┈');
+  console.log("\nFind the secret key and go to next Level\n");
 
   getAnswerAndValidate(answer);
-
   askPuzzle(puzzle, puzzleAnswer);
 }
 
@@ -99,7 +114,7 @@ function askEasyQuestions() {
 
 function startEasyLevel() {
   console.log("I see you have started the game alright go one then !\n");
-  console.log("Now you are in Easy level, let's start the game\n\n");
+  prompt("Now you are in Easy level, let's start the game\n\n");
 
   askEasyQuestions();
 }
@@ -107,24 +122,30 @@ function startEasyLevel() {
 function startMediumLevel() {
   greetForPassing();
 
-  askQuestion(mediumQuestion1, mediumQue1Answer, puzzleSolution3);
+  askQuestion(mediumQuestion1, mediumQue1Answer, puzzle3, puzzleSolution3);
 }
 
 function startHardLevel() {
   greetForPassing();
+  askQuestion(hardQuestion1, hardQue1Answer, puzzle4, puzzleSolution4);
+}
 
-  askQuestion(hardQuestion1, hardQue1Answer, puzzleSolution4);
+function printInstructionMessage() {
+  console.log(instruction);
 }
 
 function congratulatePlayer() {
-  console.log("You haven founded all the answers ");
-  console.log("Well, Done your treasure \n");
+  console.log("ʏᴏᴜ ʜᴀᴠᴇ ꜰᴏᴜɴᴅ ᴀʟʟ ᴛʜᴇ ᴀɴꜱᴡᴇʀ.");
+  prompt("ᴡᴇʟʟ ᴅᴏɴᴇ! ɴᴏᴡ ꜰᴏʀ ʏᴏᴜʀ ᴛʀᴇᴀꜱᴜʀᴇ ᴘʀᴇꜱꜱ ᴇɴᴛᴇʀ...\n");
   console.log(QUOTE_ONE_PIECE);
+
+  console.log("ᴘʟᴇᴀꜱᴇ, ᴅᴏɴ'ᴛ ʀᴇᴠᴇᴀʟ ᴛʜᴇ ᴀɴꜱᴡᴇʀꜱ ᴛᴏ ʏᴏᴜʀ ᴄᴏʟʟᴇᴀɢᴜᴇꜱ")
+  console.log("ʟᴇᴛ ᴛʜᴇᴍ ʜᴀᴠᴇ ᴛʜᴇ ꜰᴜɴ ᴀꜱ ᴡᴇʟʟ\n\n")
 }
 
 function gameStart() {
   greetPlayer();
-
+  printInstructionMessage();
   startEasyLevel();
   startMediumLevel();
   startHardLevel();
